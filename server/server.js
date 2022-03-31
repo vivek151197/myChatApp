a301125fa0104dbceb259507015033698eaf569a
vivek <vivekbk1997@gmail.com>
2022-03-31 19:02:48 +0530

    deployed app working successfully

--- server/server.js
+++ server/server.js
@@ -22,11 +22,7 @@ const _dirname1 = path.resolve()
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(_dirname1, '/client/build')))
 
-  app.get('/', (req, res) =>
-    res.sendFile(path.resolve(_dirname1, 'client', 'build', 'index.html'))
-  )
-
-  app.get('/chat', (req, res) =>
+  app.get('*', (req, res) =>
     res.sendFile(path.resolve(_dirname1, 'client', 'build', 'index.html'))
   )
 } else {

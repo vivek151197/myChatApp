cca053866044e50330199d1e0bf29b0f826e77e9
vivek <vivekbk1997@gmail.com>
2022-03-31 18:46:57 +0530

    testing

--- server/server.js
+++ server/server.js
@@ -22,11 +22,12 @@ const _dirname1 = path.resolve()
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(_dirname1, '/client/build')))
 
+  app.get('/chat', (req, res) =>
+    res.sendFile(path.resolve(_dirname1, '/client', 'build', 'index.html'))
+  )
   app.get('/', (req, res) =>
     res.sendFile(path.resolve(_dirname1, '/client', 'build', 'index.html'))
   )
-
-  app.get('/chat', (req, res) => res.send('Hi, here is the problem'))
 } else {
   app.get('/', (req, res) => {
     res.send('API is running')
